3feb3cd7b69f70c714251ac87fe0de566828ca2d
AliiiBenn <davidddpereiraaa6@gmail.com>
2024-07-05 09:11:32 +0200

    Update

---
+++ 2-annotations-type/3-annotations-generiques/3-protocols-generiques/main.py
@@ -0,0 +1,36 @@
+""" 
+
+Fichier de test personnel
+
+"""
+
+
+from typing import TypeVar, Protocol
+
+
+T = TypeVar("T", contravariant=True)
+G = TypeVar("G", covariant=True)
+
+# `Protocol` est aussi une annotation de type générique
+# qui peut être utilisée pour créer des protocols génériques. 
+class Operation(Protocol[T, G]):
+    def operation(self, elements : T) -> G: ...
+    
+    
+    
+class SommeEntiers(Operation[list[int], int]):
+    def operation(self, elements : list[int]) -> int:
+        return sum(elements)
+    
+
+class ConcatenerChaines(Operation[list[str], str]):
+    def operation(self, elements : list[str]) -> str:
+        return ''.join(elements)
+    
+    
+    
+    
+if __name__ == '__main__':
+    print(SommeEntiers().operation([1, 2, 3]))
+    print(ConcatenerChaines().operation(["1", "2", "3"]))
+    
